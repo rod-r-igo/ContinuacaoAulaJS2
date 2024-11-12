04f1614dc0e210291577f2be02662868a308b286
rod-r-igo <163418849+rod-r-igo@users.noreply.github.com>
2024-11-12 12:40:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-let peso= parseFloat(prompt("Digite seu peso: "));
-let altura= parseFloat(prompt("Informe sua altura: "));
+let peso = parseFloat(prompt("Digite o valor do peso: "));
+let altura = parseFloat(prompt("Digite o valor da altura: "));
 
-document.write("<br>");
+if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+  document.write("Digite valores válidos: ");
+} else {
+  let imc = (peso / (altura * altura)).toFixed(2);
+  document.write("Seu imc é igual a: <span style= 'color: red;'> ", imc, "</span>");
+  document.write("<br>");
 
-if(isNaN(peso)|| isNaN(altura) || peso <=0 || altura <=0){
-document.write("Digite valores validos");
-}else{
-
-    let imc = (peso/(altura*altura)).toFixed(2);
-    document.write("O IMC é igual a: ", imc);
-
-    if(imc >= 18,5 && imc <=24,9){
-    document.write("O peso está normal!!");
-    
-}else if(imc >= 25 && imc <=29,9){
-    document.write("Você esta no grau 1 de obesidade!!");
-
-}else if(imc >= 30 && imc <=34,9){
-    document.write("Você esta no grau 2 de obesidade!!");
-
-}else if(){}
-
-    
+  if (imc <= 18.5) {
+    document.write("Seu imc está abaixo do normal!!");
+  } else if (imc > 18.5 && imc <= 24.9) {
+    document.write("Seu imc está Normal!!");
+  } else if (imc > 25 && imc <= 29.9) {
+    document.write("Seu imc indica que você está em sobrepeso!!");
+  } else if (imc > 30 && imc <= 34.9) {
+    document.write("Seu imc indica que você está em Obesidade grau I!!");
+  }
 }
-
